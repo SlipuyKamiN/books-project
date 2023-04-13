703305d1c3b1302b9e1c31efa07e83ec149566a2
MozMary <viktor83pochynok@gmail.com>
2023-04-13 23:05:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,5 @@ import './js/fetchBooks';
 import './js/modal';
 import './js/auth-modal';
 import './js/scroll-up';
+
 import './js/support';
-// import './js/...';
-// import './js/...';
-// import './js/...';
-
-// import { fetchBooks } from './js/fetchBooks';
-
-// (async () => {
-//   console.dir(await fetchBooks.getCategoriesList());
-// })();
-
-// (async () => {
-//   console.dir(await fetchBooks.getBestSellers());
-// })();
-
-// (async () => {
-//   console.dir(await fetchBooks.getBooksByCategory('Business Books'));
-// })();
-
-// (async () => {
-//   console.dir(await fetchBooks.getBookById('643282b1e85766588626a080'));
-// })();
