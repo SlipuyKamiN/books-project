1a81f4dd7eecb757e4bb5d29d51886d35edab48e
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-04-13 16:17:38 +0200

    adds js darkMode

--- src/js/darkTheme.js
+++ src/js/darkTheme.js
@@ -1,20 +1,44 @@
-const modeSwitch = document.querySelector('.mode-switch');
-const body = document.querySelector('body');
 
-// Set the initial mode based on the saved value in localStorage, or use the default mode
-const savedMode = localStorage.getItem('selectedMode');
-if (savedMode) {
-  body.classList = savedMode;
-  modeSwitch.checked = savedMode === 'dark' ? true : false;
-} else {
-  body.classList = 'light';
+const switchDayNight = document.querySelector('.mode-switch');
+const bookNameArray = document.querySelectorAll('.book-name');
+
+function checkDarkModeStatus() {
+  if (localStorage.getItem('darkMode') === 'true') {
+    addDarkTheme();
+    switchDayNight.checked = true;
+  } else {
+    removeDarkTheme();
+    switchDayNight.checked = false;
+  }
 }
 
-// Add an event listener to the mode switch
-modeSwitch.addEventListener('change', function() {
-  const selectedMode = this.checked ? 'dark' : 'light';
-  body.classList = selectedMode;
-
-  // Save the selected mode in localStorage
-  localStorage.setItem('selectedMode', selectedMode);
+switchDayNight.addEventListener('change', () => {
+  if (switchDayNight.checked) {
+    addDarkTheme();
+  } else {
+    removeDarkTheme();
+  }
+  localStorage.setItem('darkMode', switchDayNight.checked);
 });
+
+function addDarkTheme() {
+  document.body.classList.add('dark');
+  document.body.style.backgroundColor = '#202024';
+  document.querySelector('header').style.backgroundColor = '#111111';
+
+  bookNameArray.forEach((book) => {
+    book.style.color = 'rgba(255, 255, 255, 0.6)';
+  });
+}
+
+function removeDarkTheme() {
+  document.body.classList.remove('dark');
+  document.body.style.backgroundColor = '#D0D0D0';
+  document.querySelector('header').style.backgroundColor = '#FFFFFF';
+
+  bookNameArray.forEach((book) => {
+    book.style.color = 'rgba(17, 17, 17, 0.6)';
+  });
+}
+
+checkDarkModeStatus();
