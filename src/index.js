aca4d836bfda1806ea8cdd7a5c595ec6ff4dfdf2
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-04-13 21:39:29 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 import './js/fetchBooks';
- import './js/darkTheme';
-// import './js/...';
-// import './js/...';
-// import './js/...';
-
-// import { fetchBooks } from './js/fetchBooks';
-
-// (async () => {
-//   console.dir(await fetchBooks.getCategoriesList());
-// })();
-
-// (async () => {
-//   console.dir(await fetchBooks.getBestSellers());
-// })();
-
-// (async () => {
-//   console.dir(await fetchBooks.getBooksByCategory('Business Books'));
-// })();
-
-// (async () => {
-//   console.dir(await fetchBooks.getBookById('643282b1e85766588626a080'));
-// })();
+import './js/modal';
+import './js/auth-modal';
+import './js/scroll-up';
+import './js/darkTheme';
