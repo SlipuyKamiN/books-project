c1b3b39a665e011704481245be5518951604c770
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-04-15 23:00:09 +0200

    try bugfix with setCurrentPage

--- src/js/header.js
+++ src/js/header.js
@@ -1,27 +1,17 @@
-
-const menuHomeEl = document.querySelector(".menu__home");
-const menuShoppingEl = document.querySelector(".menu__shopping");
+const menuHomeEl = document.querySelector('.menu__home');
+const menuShoppingEl = document.querySelector('.menu__shopping');
 
 const setCurrentPage = () => {
-const currentPageName = window.location.pathname;
-// console.log(currentPageName);
+  const currentPageName = window.location.pathname;
+  console.log(currentPageName);
 
-    if (currentPageName === '/index.html') {
-        // console.log('Ми на домашній сторінці');
-        menuHomeEl.classList.add("current");
-        return;
-    } 
-       menuShoppingEl.classList.add("current");
-        // console.log('Ми на shopping-list');
-    
+  if (currentPageName === '/index.html') {
+    console.log('Ми на домашній сторінці');
+    menuHomeEl.classList.add('current');
+    return;
+  }
+  menuShoppingEl.classList.add('current');
+  console.log('Ми на shopping-list');
 };
 
 setCurrentPage();
-
-
-
-
-
-
-
-
