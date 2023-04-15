007ed8dd1d75a7ad95bd1ec3c4944edb6f85a544
TarasDer <tderev.switch@gmail.com>
2023-04-15 19:50:28 +0300

    added something

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,25 +1,55 @@
 import transformer from 'parcel-transformer-hbs';
 import { fetchBooks } from '../js/fetchBooks';
+import { makeMarkupGategory, showAllCategories } from './home/allCategories';
+import { handleImgClick } from './home/allCategories';
+import { handleModalWindow } from './modal';
+import { addEventListenerForBook } from './home/allCategories';
+// import { checkLengthBookTitle } from './home/allCategories';
+import { addEventListenerForBook } from './home/allCategories';
+import { showAllCategories } from './home/allCategories';
 
 const listEl = document.querySelector('.categories-list-js');
+const mainListEl = document.querySelector('.main__list-js');
+const mainTitle = document.querySelector('.main__title-js');
+const allCategoriesBtn = document.querySelector('.all-categories-btn');
 
-const func = async () => {
+const createCategoryList = async () => {
   const categoriesList = await fetchBooks.getCategoriesList();
 
   const makeNewButtons = categoriesList
     .map(
       category =>
-        `<li class= 'categories-list__button'> <button> ${category.list_name}</button> </li>`
+        `<li class= 'categories-list__button'> <button>${category.list_name}</button> </li>`
     )
     .join('');
   listEl.insertAdjacentHTML('beforeend', makeNewButtons);
 };
-func();
+createCategoryList();
+
+allCategoriesBtn.addEventListener('click', showAllCategories);
+
+export const drawCategory = async name => {
+  const books = await fetchBooks.getBooksByCategory(name);
+  const markup = makeMarkupGategory(books);
+  const titleArr = name.split(' ');
+  const titleFirstPart = titleArr.slice(0, titleArr.length - 1).join(' ');
+  const titleLastPart = titleArr.slice(titleArr.length - 1).join();
+  mainTitle.innerHTML = `${titleFirstPart}<span class="main__title--color-purple"> ${titleLastPart}</span>`;
+  mainListEl.innerHTML = markup;
+  mainListEl.classList.add('card-set');
+};
 
 listEl.addEventListener('click', markup);
 
+let previ = '';
+
 function markup(ev) {
   let title = ev.target.textContent;
-  const mainTitleEl = document.querySelector('.main__title');
-  mainTitleEl.textContent = `${title}`;
+  drawCategory(title);
+
+  if (previ !== '') {
+    previ.style.textTransform = 'none';
+  }
+  ev.target.style.textTransform = 'uppercase';
+  previ = ev.target;
 }
