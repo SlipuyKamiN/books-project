369d246b3ba3c9bee2653b61bf8fe864dacfabbd
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-04-16 00:05:03 +0200

    resolve little issues

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -3,7 +3,7 @@ import './js/modal';
 import './js/auth-modal';
 import './js/scroll-up';
 import './js/darkTheme';
-import './js/categories';
+// import './js/categories';
 import './js/shopping-list/getShoppingList';
 import './js/header';
 import './js/support';
