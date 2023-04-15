e3d31a18658df0752b8d8893f708ec508548aa94
Viasheslav Rashnik <vasheslav1000@gmail.com>
2023-04-15 03:43:05 +0300

    shoppingList

--- src/js/shopping-list/getShoppingList.js
+++ src/js/shopping-list/getShoppingList.js
@@ -1,9 +1,24 @@
+import shop1 from '../../images/modal/image-1@1x.png';
+import shop2 from '../../images/modal/image-2@1x.png';
+import shop3 from '../../images/modal/image-3@1x.png';
+
 const shoppingList = document.querySelector('.shopping-main__list-js');
 const button = document.querySelector('.shopping-button-js');
 const emptyList = document.querySelector('.empty-list');
 
 let dataBooks = localStorage.getItem('books-data');
 dataBooks = JSON.parse(dataBooks);
+console.log(dataBooks);
+
+const amazonUrl = dataBooks
+  .find(obj => obj.buy_links.some(link => link.name === 'Amazon'))
+  .buy_links.find(link => link.name === 'Amazon').url;
+const appleBooksUrl = dataBooks
+  .find(obj => obj.buy_links.some(link => link.name === 'Apple Books'))
+  .buy_links.find(link => link.name === 'Apple Books').url;
+const barnesAndNobleUrl = dataBooks
+  .find(obj => obj.buy_links.some(link => link.name === 'Barnes and Noble'))
+  .buy_links.find(link => link.name === 'Barnes and Noble').url;
 
 const cardMarkup = createCardMarkup(dataBooks);
 
@@ -26,94 +41,123 @@ function createCardMarkup(dataBooks) {
     <path d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
             </button>
-            <div class="flex-box">
-              <figure class="shopping__box">
-                <picture>
-                  <img
-                    src="${book.book_image}"
-                    alt=""
-                    class="shopping__img"
-                    loading="lazy"
-                  />
-                </picture>
-                <figcaption class='shopping__author shopping-box-mobile'>${book.author}</figcaption>
-              </figure>
-              <div class="flex-box__text">
-                <h2 class="shopping__title">${book.title}</h2>
-                <p class="shopping__titleText">Hardcover fiction</p>
-                <div class='shops-box'>
-                <ul class="shops-list shopping-box-mobile">
-                  <li>
-                    <a
-                      class="shops-list__link"
-                      href=""
-                      aria-label=""
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="shops-list__link"
-                      href=""
-                      aria-label=""
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="shops-list__link"
-                      href=""
-                      aria-label=""
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                    </a>
-                  </li>
-                </ul>
+            <div class="flex-box-mobale">
+                <figure class="shopping__box">
+                  <picture>
+                    <img
+                      src="${book.book_image}"
+                      alt="book"
+                      class="shopping__img"
+                      loading="lazy"
+                    />
+                  </picture>
+                  <figcaption class='shopping__author shopping__author-mobile'>${book.author}</figcaption>
+                </figure>
+
+                <div class="flex-box__text-mobale">
+                  <h2 class="shopping__title">${book.title}</h2>
+                  <p class="shopping__titleText">Hardcover fiction</p>
+                  <div class='shops-box shops-box-mobile'>
+                    <ul class="shops-list">
+                    <li>
+                      <a
+                        class="shops-list__link"
+                        href="${amazonUrl}"
+                        aria-label=""
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                      <img class='shops-list__img' src="${shop1}" width="48" height="15" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="shops-list__link"
+                        href="${appleBooksUrl}"
+                        aria-label=""
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                      <img class='shops-list__img' src="${shop2}" width="28" height="27" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="shops-list__link"
+                        href="${barnesAndNobleUrl}"
+                        aria-label=""
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                      <img class='shops-list__img' src="${shop3}" width="32" height="30" />
+                      </a>
+                    </li>
+                  </ul>
+                  </div>
+                  </div>
                 </div>
-              </div>
+                <p class='shopping__text'>${book.description}</p>
             </div>
-            <p class='shopping__text'>${book.description}</p>
-            <div class="shopping-box-tablet">
-            <p class='shopping__author-mobile'>${book.author}</p>
-                <ul class="shops-list">
-                  <li>
-                    <a
-                      class="shops-list__link"
-                      href=""
-                      aria-label=""
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="shops-list__link"
-                      href=""
-                      aria-label=""
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="shops-list__link"
-                      href=""
-                      aria-label=""
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                    </a>
-                  </li>
-                </ul>
-                </div>
-          </li>`;
+      </li>
+      
+      <li class="shopping__item shopping__item-tablet" id=${book._id}>
+            <button type="button" data-action='delete' value="remove" class="shopping-button shopping-button-js">
+              <svg class="shopping-button__icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+            </button>
+        <div class='flex-box-tablet'>
+                    <img
+                      src="${book.book_image}"
+                      alt="book"
+                      class="shopping__img"
+                      loading="lazy"
+                    />
+            <div class="flex-box__text-tablet">
+                  <h2 class="shopping__title">${book.title}</h2>
+                  <p class="shopping__titleText">Hardcover fiction</p>
+                  <p class='shopping__text'>${book.description}</p>
+                  <div class="shopping-box-tablet">
+                    <p class='shopping__author'>${book.author}</p>
+                    <ul class="shops-list">
+                      <li>
+                        <a
+                          class="shops-list__link"
+                          href=""
+                          aria-label=""
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                        <img src="${shop1}" width="48" height="15" />
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          class="shops-list__link"
+                          href=""
+                          aria-label=""
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                        <img src="${shop2}" width="28" height="27" />
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          class="shops-list__link"
+                          href=""
+                          aria-label=""
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                        <img src="${shop3}" width="32" height="30" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+             </div> 
+        </div>
+      </li>`;
       })
       .join('');
   }
