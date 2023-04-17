3d753c545f5b557f59ec2eb130ee57d58d6ff26d
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-04-17 14:47:12 +0300

    модальне меню1

--- src/js/header.js
+++ src/js/header.js
@@ -16,3 +16,78 @@ const setCurrentPage = () => {
 };
 
 setCurrentPage();
+
+
+
+
+
+//   <!-- МОДАЛЬНЕ  ВІКНО МЕНЮ  -->
+
+
+
+  
+
+
+
+
+
+
+const modalBoxEl = document.querySelector(".data-modal");
+
+const openModalBtnEl = document.querySelector(".js-open-menu");
+
+const iconBurgerEl = document.querySelector(".icon-burger");
+
+const iconHrefEl = document.querySelector(".icon-href");
+
+const iconXCloseEl = document.querySelector(".icon-x-close");
+
+
+  openModalBtnEl.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+      modalBoxEl.classList.toggle("is-hidden");
+    
+  
+      iconHrefEl.setAttribute('href', "./icons.adfc4680.svg#icon-x-close");
+
+    }
+
+
+
+
+
+
+
+
+
+
+      //  відкриття/закриття  модального меню
+
+// const openModalBtnEl = document.querySelector(".js-open-menu");
+// const closeModalBtnEl = document.querySelector(".js-close-menu");
+// const iconBurgerEl = document.querySelector(".icon-burger");
+
+// const modalBoxEl = document.querySelector(".data-modal");
+
+  // openModalBtnEl.addEventListener("click", toggleModal);
+  // closeModalBtnEl.addEventListener("click", toggleModal);
+  
+  //   function toggleModal() {
+  //     modalBoxEl.classList.toggle("is-hidden");
+  //   }
+
+    
+
+//   openModalBtnEl.addEventListener("click", toggleModal);
+
+  
+//     function toggleModal() {
+//       modalBoxEl.classList.toggle("is-hidden");
+
+//       openModalBtnEl.innerHTML = `<button class="modal__btn-close js-close-menu is-hidden ">
+//     <svg class="icon-x-close" width="28" height="28">
+//       <use href="./images/icons.svg#icon-x-close"></use>
+//     </svg>
+//   </button>`;
+//     }
