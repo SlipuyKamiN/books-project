d3b6e97a05d497b4104d734c311e3cb74c374a9d
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-04-17 09:58:58 +0200

    adds dark mode styles

--- src/js/darkTheme.js
+++ src/js/darkTheme.js
@@ -1,63 +1,63 @@
-const switchDayNight = document.querySelector('.mode-switch');
-const books = document.querySelectorAll('.book-name, .category-books__name, .load-more-js, .main__title-js, .category-books__title + .category-books__author');
+// const switchDayNight = document.querySelector('.mode-switch');
+// const books = document.querySelectorAll('.book-name, .category-books__name, .load-more-js, .main__title-js, .category-books__title + .category-books__author');
 
-function checkDarkModeStatus() {
-  if (localStorage.getItem('darkMode') === 'true') {
-    addDarkTheme();
-    switchDayNight.checked = true;
-  } else {
-    removeDarkTheme();
-    switchDayNight.checked = false;
-  }
-}
-
-switchDayNight.addEventListener('change', () => {
-  if (switchDayNight.checked) {
-    addDarkTheme();
-  } else {
-    removeDarkTheme();
-  }
-  localStorage.setItem('darkMode', switchDayNight.checked);
-});
-
-function addDarkTheme() {
-  document.body.classList.add('dark');
-  books.forEach(book => {
-    book.style.color = '$white-color';
-  });
-}
-
-function removeDarkTheme() {
-  document.body.classList.remove('dark');
-  document.body.style.backgroundColor = '#f6f6f6';
-  document.querySelector('header').style.backgroundColor = '#FFFFFF';
-
-  bookNameArray.forEach(book => {
-    book.style.color = 'rgba(17, 17, 17, 0.6)';
-  });
-}
-
-checkDarkModeStatus();
-
-
-
-// const modeSwitch = document.querySelector('.mode-switch');
-// const body = document.querySelector('body');
-
-// // Set the initial mode based on the saved value in localStorage, or use the default mode
-// const savedMode = localStorage.getItem('selectedMode');
-// if (savedMode) {
-//   body.classList = savedMode;
-//   modeSwitch.checked = savedMode === 'dark' ? true : false;
-// } else {
-//   body.classList = 'light';
+// function checkDarkModeStatus() {
+//   if (localStorage.getItem('darkMode') === 'true') {
+//     addDarkTheme();
+//     switchDayNight.checked = true;
+//   } else {
+//     removeDarkTheme();
+//     switchDayNight.checked = false;
+//   }
 // }
 
-// // Add an event listener to the mode switch
-// modeSwitch.addEventListener('change', function() {
-//   const selectedMode = this.checked ? 'dark' : 'light';
-//   body.classList = selectedMode;
-
-//   // Save the selected mode in localStorage
-//   localStorage.setItem('selectedMode', selectedMode);
+// switchDayNight.addEventListener('change', () => {
+//   if (switchDayNight.checked) {
+//     addDarkTheme();
+//   } else {
+//     removeDarkTheme();
+//   }
+//   localStorage.setItem('darkMode', switchDayNight.checked);
 // });
+
+// function addDarkTheme() {
+//   document.body.classList.add('dark');
+//   books.forEach(book => {
+//     book.style.color = '$white-color';
+//   });
+// }
+
+// function removeDarkTheme() {
+//   document.body.classList.remove('dark');
+//   document.body.style.backgroundColor = '#f6f6f6';
+//   document.querySelector('header').style.backgroundColor = '#FFFFFF';
+
+//   bookNameArray.forEach(book => {
+//     book.style.color = 'rgba(17, 17, 17, 0.6)';
+//   });
+// }
+
+// checkDarkModeStatus();
+
+
+
+const modeSwitch = document.querySelector('.mode-switch');
+const body = document.querySelector('body');
+
+// Set the initial mode based on the saved value in localStorage, or use the default mode
+const savedMode = localStorage.getItem('selectedMode');
+if (savedMode) {
+  body.classList = savedMode;
+  modeSwitch.checked = savedMode === 'dark' ? true : false;
+} else {
+  body.classList = 'light';
+}
+
+// Add an event listener to the mode switch
+modeSwitch.addEventListener('change', function() {
+  const selectedMode = this.checked ? 'dark' : 'light';
+  body.classList = selectedMode;
+
+  // Save the selected mode in localStorage
+  localStorage.setItem('selectedMode', selectedMode);
+});
