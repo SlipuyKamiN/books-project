294439b23ddcdb2acd2473bc5afb21a7ff7c0d56
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-04-17 22:19:34 +0300

    стилізація кнопок дроп-меню

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,12 @@
 import openCloseIcon from '../images/icons.svg';
 
 
+// ВИДІЛЕННЯ ЖОВТИМ НАЗВУ ПОТОЧНОЇ СТОРІНКИ (меню в хедері)
 const menuHomeEl = document.querySelector('.menu__home');
 const menuShoppingEl = document.querySelector('.menu__shopping');
-const authorisedUserBtn = document.querySelector('.user-btn-js');
-const logOutBtn = document.querySelector('.log-out-btn-js');
 
 const setCurrentPage = () => {
   const currentPageName = window.location.pathname;
-  // console.log(currentPageName);
 
   if (
     currentPageName === '/index.html' ||
@@ -26,33 +24,14 @@ setCurrentPage();
 
 
 
-//   <!-- МОДАЛЬНЕ  ВІКНО МЕНЮ  -->
-
+//   ВІДКРИТТЯ/ЗАКРИТТЯ МОДАЛКИ ДЛЯ МОБ. ВЕРСІЇ
 
 const modalBoxEl = document.querySelector(".data-modal");
-
 const openModalBtnEl = document.querySelector(".js-open-menu");
-
 const iconHrefEl = document.querySelector(".icon-href");
-
-const authModalOpenEl = document.querySelector(".auth__modal-open-js.mobile");
 
 
   openModalBtnEl.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//   if (modalBoxEl.classList.contains('is-hidden')) {
-//     modalBoxEl.classList.remove('is-hidden');
-//     iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-x-close`);
-
-    
-//     return;
-//   } else {
-//     modalBoxEl.classList.add('is-hidden');
-//     iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-burger`);
-//   }
-// }
-
 
   function toggleModal() {
   if (modalBoxEl.classList.contains('is-hidden')) {
@@ -67,5 +46,24 @@ const authModalOpenEl = document.querySelector(".auth__modal-open-js.mobile");
 
 
 
+// ВИДІЛЕННЯ ЖОВТИМ, НАЗВУ ПОТОЧНОЇ СТОРІНКИ (меню в МОДАЛЦІ)
+
+const dropMenuHomeEl = document.querySelector('.drop-menu__home');
+const dropMenuShoppingEl = document.querySelector('.drop-menu__shopping');
+
+const dropSetCurrentPage = () => {
+const dropCurrentPageName = window.location.pathname;
+
+  if (
+    dropCurrentPageName === '/index.html' ||
+    dropCurrentPageName === '/books-project/index.html'
+  ) {
+    dropMenuHomeEl.classList.add('current');
+    return;
+  }
+  dropMenuShoppingEl.classList.add('current');
+};
+
+dropSetCurrentPage();
 
 
