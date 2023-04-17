96d4cb7837df36b04d02bb820cfc4beffb4ea332
Viasheslav Rashnik <vasheslav1000@gmail.com>
2023-04-18 00:02:50 +0300

    bugfix/shoppingList

--- src/js/shopping-list/getShoppingList.js
+++ src/js/shopping-list/getShoppingList.js
@@ -1,42 +1,46 @@
 const shoppingList = document.querySelector('.shopping-main__list-js');
 const emptyList = document.querySelector('.empty-list');
 
+let dataBooks = localStorage.getItem('books-data');
+
 try {
-  let dataBooks = localStorage.getItem('books-data');
   dataBooks = JSON.parse(dataBooks);
+} catch (error) {
+  console.log(error);
+}
 
-  if (Array.isArray(dataBooks)) {
-    const cardMarkup = createCardMarkup(dataBooks);
-    shoppingList.insertAdjacentHTML('beforeend', cardMarkup);
-    shoppingList.addEventListener('click', removeCardMarkup);
-  } else {
+if (Array.isArray(dataBooks)) {
+  const cardMarkup = createCardMarkup(dataBooks);
+  shoppingList.insertAdjacentHTML('beforeend', cardMarkup);
+  shoppingList.addEventListener('click', removeCardMarkup);
+} else {
+  shoppingList.classList.add('visually-hidden');
+  emptyList.classList.remove('visually-hidden');
+}
+
+function createCardMarkup(dataBooks) {
+  if (!dataBooks || dataBooks.length === 0) {
     shoppingList.classList.add('visually-hidden');
-    emptyList.classList.remove('visually-hidden');
   }
+  if (dataBooks.length > 0) {
+    emptyList.classList.add('visually-hidden');
+    shoppingList.classList.remove('visually-hidden');
 
-  function createCardMarkup(dataBooks) {
-    if (!dataBooks || dataBooks.length === 0) {
-      shoppingList.classList.add('visually-hidden');
-    }
-    if (dataBooks.length > 0) {
-      emptyList.classList.add('visually-hidden');
-      shoppingList.classList.remove('visually-hidden');
+    return dataBooks
+      .map(book => {
+        const amazonUrl = book.buy_links.find(
+          link => link.name === 'Amazon'
+        ).url;
+        const appleBooksUrl = book.buy_links.find(
+          link => link.name === 'Apple Books'
+        ).url;
+        const barnesAndNobleUrl = book.buy_links.find(
+          link => link.name === 'Barnes and Noble'
+        ).url;
 
-      return dataBooks
-        .map(book => {
-          const amazonUrl = book.buy_links.find(
-            link => link.name === 'Amazon'
-          ).url;
-          const appleBooksUrl = book.buy_links.find(
-            link => link.name === 'Apple Books'
-          ).url;
-          const barnesAndNobleUrl = book.buy_links.find(
-            link => link.name === 'Barnes and Noble'
-          ).url;
-
-          return `<li class="shopping__item shopping__item-mobile" data-id=${
-            book._id
-          }>
+        return `<li class="shopping__item shopping__item-mobile" data-id=${
+          book._id
+        }>
             <button type="button" data-action='delete' value="remove" class="shopping-button shopping-button-js">
               <svg class="shopping-button__icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
@@ -171,6 +175,7 @@ try {
                         ${require('../../images/modal/image-3@2x.png')} 64w,
                         "
                         sizes="32px" src="${require('../../images/modal/image-3@1x.png')}" width="32" height="30" />
+                        </a>
                       </li>
                     </ul>
                   </div>
@@ -178,34 +183,31 @@ try {
         </div>
       </div>
       </li>`;
-        })
-        .join('');
-    }
+      })
+      .join('');
+  }
+}
+
+function removeCardMarkup(event) {
+  if (event.target.dataset.action !== 'delete') {
+    return;
   }
 
-  function removeCardMarkup(event) {
-    if (event.target.dataset.action !== 'delete') {
-      return;
-    }
+  const parentNode = event.target.closest('.shopping__item');
+  const bookToRemoveId = parentNode.dataset.id;
 
-    const parentNode = event.target.closest('.shopping__item');
-    const bookToRemoveId = parentNode.dataset.id;
+  dataBooks = dataBooks.filter(book => book._id !== bookToRemoveId);
 
-    dataBooks = dataBooks.filter(book => book._id !== bookToRemoveId);
-
-    saveToLocalStorage();
-    parentNode.remove();
-    if (!dataBooks || dataBooks.length === 0) {
-      emptyList.classList.remove('visually-hidden');
-    }
-    if (dataBooks.length > 0) {
-      emptyList.classList.add('visually-hidden');
-    }
+  saveToLocalStorage();
+  parentNode.remove();
+  if (!dataBooks || dataBooks.length === 0) {
+    emptyList.classList.remove('visually-hidden');
   }
-
-  function saveToLocalStorage() {
-    localStorage.setItem('books-data', JSON.stringify(dataBooks));
+  if (dataBooks.length > 0) {
+    emptyList.classList.add('visually-hidden');
   }
-} catch (error) {
-  console.log(error);
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem('books-data', JSON.stringify(dataBooks));
 }
