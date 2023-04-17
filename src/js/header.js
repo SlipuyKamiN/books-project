acc598ce1591b0d676d25308c238ce3aafb5a126
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-04-18 00:12:23 +0300

    Перероблена модалка

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,8 @@
 import openCloseIcon from '../images/icons.svg';
 
 
+
+
 // ВИДІЛЕННЯ ЖОВТИМ НАЗВУ ПОТОЧНОЇ СТОРІНКИ (меню в хедері)
 const menuHomeEl = document.querySelector('.menu__home');
 const menuShoppingEl = document.querySelector('.menu__shopping');
@@ -22,8 +24,6 @@ setCurrentPage();
 
 
 
-
-
 //   ВІДКРИТТЯ/ЗАКРИТТЯ МОДАЛКИ ДЛЯ МОБ. ВЕРСІЇ
 
 const modalBoxEl = document.querySelector(".data-modal");
@@ -31,16 +31,21 @@ const openModalBtnEl = document.querySelector(".js-open-menu");
 const iconHrefEl = document.querySelector(".icon-href");
 
 
+// const mobbEl = document.querySelector('.mob-header');
+
   openModalBtnEl.addEventListener("click", toggleModal);
 
   function toggleModal() {
   if (modalBoxEl.classList.contains('is-hidden')) {
     modalBoxEl.classList.remove('is-hidden');
     iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-x-close`);
+    // mobbEl.classList.remove("mob-header");
+
     return;
   } else {
     modalBoxEl.classList.add('is-hidden');
     iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-burger`);
+    // mobbEl.classList.add("mob-header");
   }
 }
 
