b905525c3aa8902841919962feb5b777a744292c
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-04-17 17:21:35 +0300

    список книжок

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,6 @@
+import openCloseIcon from '../images/icons.svg';
+
+
 const menuHomeEl = document.querySelector('.menu__home');
 const menuShoppingEl = document.querySelector('.menu__shopping');
 const authorisedUserBtn = document.querySelector('.user-btn-js');
@@ -26,75 +29,28 @@ setCurrentPage();
 //   <!-- МОДАЛЬНЕ  ВІКНО МЕНЮ  -->
 
 
-
-  
-
-
-
-
-
-
 const modalBoxEl = document.querySelector(".data-modal");
 
 const openModalBtnEl = document.querySelector(".js-open-menu");
 
-const iconBurgerEl = document.querySelector(".icon-burger");
-
 const iconHrefEl = document.querySelector(".icon-href");
-
-const iconXCloseEl = document.querySelector(".icon-x-close");
 
 
   openModalBtnEl.addEventListener("click", toggleModal);
 
-    function toggleModal() {
-      modalBoxEl.classList.toggle("is-hidden");
-    
-  
-      iconHrefEl.setAttribute('href', "./icons.adfc4680.svg#icon-x-close");
-
-    }
-
-
-
-
-
+  function toggleModal() {
+  if (modalBoxEl.classList.contains('is-hidden')) {
+    modalBoxEl.classList.remove('is-hidden');
+    iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-x-close`);
+    return;
+  } else {
+    modalBoxEl.classList.add('is-hidden');
+    iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-burger`);
+  }
+}
 
 
 
 
 
-      //  відкриття/закриття  модального меню
 
-// const openModalBtnEl = document.querySelector(".js-open-menu");
-// const closeModalBtnEl = document.querySelector(".js-close-menu");
-// const iconBurgerEl = document.querySelector(".icon-burger");
-
-// const modalBoxEl = document.querySelector(".data-modal");
-
-  // openModalBtnEl.addEventListener("click", toggleModal);
-  // closeModalBtnEl.addEventListener("click", toggleModal);
-  
-  //   function toggleModal() {
-  //     modalBoxEl.classList.toggle("is-hidden");
-  //   }
-
-    
-
-//   openModalBtnEl.addEventListener("click", toggleModal);
-
-  
-//     function toggleModal() {
-//       modalBoxEl.classList.toggle("is-hidden");
-
-//       openModalBtnEl.innerHTML = `<button class="modal__btn-close js-close-menu is-hidden ">
-//     <svg class="icon-x-close" width="28" height="28">
-//       <use href="./images/icons.svg#icon-x-close"></use>
-//     </svg>
-//   </button>`;
-//     }
-const handleUserBtnClick = () => {
-  logOutBtn.classList.toggle('is-hidden');
-};
-
-authorisedUserBtn.addEventListener('click', handleUserBtnClick);
